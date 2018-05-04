8ea09685b77ddbca3954ca52187854d2a7f7a14c
Sean Stephenson <Sean.M.Stephenson@espn.com>
2018-05-04 09:11:36 -0700

    add create react app

---
+++ packages/headline/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// const Nav = () => (
+//   <header className='App-header'>
+//     <h1 className='App-title'>Hello World Lerna React</h1>
+//   </header>
+// )
+
+const Nav = React.createElement('div', null,
+  React.createElement('h1', null, 'Hello World')
+)
+
+export default Nav
