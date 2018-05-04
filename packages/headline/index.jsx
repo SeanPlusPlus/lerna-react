efc7f571c529e08e364f19c128968351e3d9b826
Sean Stephenson <Sean.M.Stephenson@espn.com>
2018-05-04 09:15:25 -0700

    add headline component to app

--- packages/headline/index.jsx
+++ packages/headline/index.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 // const Nav = () => (
 //   <header className='App-header'>
-//     <h1 className='App-title'>Hello World Lerna React</h1>
+//     <h1 className='App-title'>Hello Lerna React</h1>
 //   </header>
 // )
 
 const Nav = React.createElement('div', null,
-  React.createElement('h1', null, 'Hello World')
+  React.createElement('h1', null, 'Hello Lerna')
 )
 
 export default Nav
